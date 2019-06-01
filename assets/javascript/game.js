bd972f35c3140bd73dbd8f8af09ab45e7729be7e
Christian Pak <iamchristianpak@gmail.com>
2019-06-01 17:01:49 -0600

    made a letter checker function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,15 @@
 var characters = ['bowser', 'waluigi', 'koopalings', 'birdo', 'rosalina', 'toad', 'yoshi']
 
 // empty variables to store later
-var lettersguessed = [];
 var ranCharacter = "";
 var blanks = 0;
 var correctblanks = [];
 var lettersofword = [];
+var lettersguessed = [];
 
 // counter variables
 var wins = 0;
-var guessesremaining = 9;
+var guessesremaining = 10;
 
 //html shortcuts
 var winstext = document.getElementById("wins");
@@ -18,15 +18,15 @@ var currentwordtext = document.getElementById("currentword")
 var guessesremainingtext = document.getElementById("guessesremaining")
 var lettersguessedtext = document.getElementById("lettersguessed")
 
-// function reset(){
-//     lettersguessed = [];
-//     guessesremaining = 9;
-//     correctblanks = [];
-//     game();
-// }
+function reset(){
+    lettersguessed = [];
+    guessesremaining = 9;
+    correctblanks = [];
+    gamesetup();
+}
 
 // game start function
-function game() {
+function lgamesetup() {
     // chooses a character
     var ranCharacter = characters(Math.floor(Math.random() * characters.length));
 
@@ -35,12 +35,13 @@ function game() {
 
     // stores/holds the amount of "length" as an integer
     blanks = lettersofword.length;
-
+ 
     // creates a loop generating the amuount of blanks needed to fill
-    for (i = 0; i < blanks; i++) {
+    for (var i = 0; i < blanks; i++) {
         // for each blank, it pushes a underscore
         correctblanks.push("_")
     }
+    // prints out the blanks
     currentword.innerHTML = " " + correctblanks.join(" ");
 
 
@@ -50,6 +51,34 @@ function game() {
     console.log(blanks)
     console.log(correctblanks)
 }
+
+//IfElse, to see if letter selected matches random word
+function checkLetters(letter) {
+    var lettersinword = false;
+
+    //if the generated randomword is equal to the letter entered... then variable is true
+    for (var i = 0; i < blanks; i++){
+        if(ranCharacter[i] == letter){
+            lettersinword = true;
+        }
+    }
+    
+    // if true, the letter will remove blanks and replace
+    if(lettersinword){
+        for (var i = 0; i < blanks; i++){
+            if(ranCharacter[i] == letter){
+                correctblanks = letter;
+            }
+        }
+        // the guess will go down and the guessed letter will be outputted
+    }else {
+        guessesremaining--;
+        lettersguessed.push(lettersguessed);
+    }
+    console.log(correctblanks);
+}    
+
+
 
 
 // // audio and pictures
